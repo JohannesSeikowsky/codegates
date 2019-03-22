96df480c50e8ae6b90b1afcb0a390ac7952ed4b7
JohannesSeikowsky <joseikowsky@googlemail.com>
2019-03-22 15:19:36 +0000

    init

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   layout "plain", only: [:student_comm_thanks]
-  layout "practice", only: [:page_three]
+  layout "practice", only: [:page_three, :page_five, :page_eight, :page_ten, :page_twelve, :page_fourteen]
 
   def page_one
   end
@@ -26,6 +26,23 @@ class PagesController < ApplicationController
   def page_eight
   end
 
+  def page_nine
+  end
+
+  def page_ten
+  end
+
+  def page_eleven
+  end
+
+  def page_twelve
+  end
+
+  def page_thirteen
+  end
+
+  def page_fourteen
+  end
 
   def student_comm_thanks
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
   get 'lesson_six', to: 'pages#page_six', as: 'page_six'
   get 'lesson_seven', to: 'pages#page_seven', as: 'page_seven'
   get 'lesson_eight', to: 'pages#page_eight', as: 'page_eight'
+  get 'lesson_nine', to: 'pages#page_nine', as: 'page_nine'
+  get 'lesson_ten', to: 'pages#page_ten', as: 'page_ten'
+  get 'lesson_eleven', to: 'pages#page_eleven', as: 'page_eleven'
+  get 'lesson_twelve', to: 'pages#page_twelve', as: 'page_twelve'
+  get 'lesson_thirteen', to: 'pages#page_thirteen', as: 'page_thirteen'
+  get 'lesson_fourteen', to: 'pages#page_fourteen', as: 'page_fourteen'
+  get 'lesson_fifteen', to: 'pages#page_fifteen', as: 'page_fifteen'
 
   post 'communication', to: 'student_communications#create', as: 'student_communication'
 
