3b9019d431337c087655ed04f9aaaaea0ec8f812
JohannesSeikowsky <joseikowsky@googlemail.com>
2019-06-05 00:40:42 +0100

    ..

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,44 +2,53 @@ class PagesController < ApplicationController
   layout "practice", only: [:page_three, :page_five, :page_eight, :page_ten, :page_twelve, :page_fourteen]
 
   def page_one
+    @current = "lesson_one"
     render :layout => 'plain'
   end
 
   def page_two
+    @current = "lesson_two"
   end
 
   def page_three
+    @current = "first_practice"
     render :layout => 'plain'
   end
 
   def page_four
+    @current = "lesson_four"
   end
 
   def page_five
   end
 
   def page_six
+    @current = "lesson_six"
   end
 
   def page_seven
+    @current = "lesson_seven"
   end
 
   def page_eight
   end
 
   def page_nine
+    @current = "lesson_nine"
   end
 
   def page_ten
   end
 
   def page_eleven
+    @current = "lesson_eleven"
   end
 
   def page_twelve
   end
 
   def page_thirteen
+    @current = "lesson_thirteen"
   end
 
   def page_fourteen
